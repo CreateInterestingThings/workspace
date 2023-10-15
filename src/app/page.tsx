8249d2644c199ada729da5fe0607b8564382a3e4
zengxian <zengxian@zhuanzhuan.com>
2023-10-15 18:10:30 +0800

    feat: 🎸 initialize

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-6xl mb-6">CREATE INTERESTING THINGS</h1>
+        <h2 className="text-2xl underline cursor-pointer">WORKSPACE</h2>
+      </div>
+    </main>
+  );
+}

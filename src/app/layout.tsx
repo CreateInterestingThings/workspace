105d5b6e50a5bcb711d3b764b44b93a8365c3475
shanezzzz <shanezeng77@gmail.com>
2023-11-08 01:26:49 +0800

    feat: 🎸 add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Workspace',
   description: 'Create interesting things',
+  icons: {
+    icon: '/logo.png'
+  }
 }
 
 export default function RootLayout({

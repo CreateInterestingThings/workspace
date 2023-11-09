9315b4f8dc3f632cd071af94e16c62fc777f29d5
shanezzzz <shanezeng77@gmail.com>
2023-11-10 02:00:13 +0800

    feat: 🎸 add login.tsx, install nextui tailwindcss favicon.ico

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@nextui-org/button';
+
+const Login = () => {
+  return <div className='text-3xl'>
+    <Button>LOGIN</Button>
+  </div>
+}
+
+export default Login

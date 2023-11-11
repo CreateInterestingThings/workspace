2cff1c6df4c97c0d897c59b2d1742b889ae2af94
shanezzzz <shanezeng77@gmail.com>
2023-11-11 23:34:57 +0800

    feat: 🎸 formatted code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-6xl mb-6">CREATE INTERESTING THINGS</h1>
-        <h2 className="text-2xl underline cursor-pointer">WORKSPACE</h2>
+    <main className='flex min-h-screen items-center justify-center'>
+      <div className='text-center'>
+        <h1 className='mb-6 text-6xl'>CREATE INTERESTING THINGS</h1>
+        <h2 className='cursor-pointer text-2xl underline'>WORKSPACE</h2>
       </div>
     </main>
   );
